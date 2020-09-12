bfe1e38b52bbec6d67f56d068893721d42811261
jmartinezar <jmartinezar@unal.edu.co>
2020-09-12 11:05:27 +0000

    Primer punto terminado

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # include <iostream>
 # include <cstdlib>
 
@@ -12,7 +11,7 @@ int N = std::atoi(argv[1]);
 int maxval = -1, n0max = -1;
 for (int ii = 1; ii <= N; ++ii) 
 {
-if(maximum_collatz(ii)>=maxval)
+if(maximum_collatz(ii)>maxval)
 {
   maxval=maximum_collatz(ii);
   n0max=ii;
@@ -40,53 +39,4 @@ if(m>=x){
 m=next_collatz_term(m);
 }
 return x;
-=======
-#include <iostream>
-
-int is_prime (int a);
-int all_prime (int a, int b);
-
-int main() {
-  int a, b;
-std::cout << "Imprime la cantidad de números primos desde \n";
-std::cin >> a;
-std::cout << "hasta \n";
-std::cin >> b;
-all_prime(a, b);
-}
-
-int is_prime (int a)
-{
-  int b=1, ii;
-  if(a==2)
-  {
-    return b;
-  }
-  else 
-  {
-for(ii=2; ii<a; ++ii)
-{
-  if(a%ii==0)
-  {
-    b=0;
-    break;
-  }
-}
-return b;
-  }
-}
-
-int all_prime (int a, int b)
-{
-int ii, p, su=0;
-for(ii=a; ii<=b; ++ii)
-{
- p = is_prime(ii);
- if(p==1)
- {
-  ++su;
- }
-}
-std::cout << su << "\n";
->>>>>>> b2b6fa061c78652544fc2f94ca94c2d3960520ac
 }
